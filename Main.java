bde49bb8f6a6a273a9f0296818e2061754c02919
Victor Magosso <aplicativosmagosso@gmail.com>
2020-06-27 21:53:43 -0300

    Arquivo executável

---
+++ Main.java
@@ -0,0 +1,9 @@
+package br.victormagosso;
+
+public class Main {
+    public static void main(String ... args){
+        Bhaskara baskara = new Bhaskara();
+
+        baskara.setVisible(true);
+    }
+}
