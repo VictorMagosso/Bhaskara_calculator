63ab543b00ce3cdaed2aa85d1401603c928dde72
Victor Magosso <aplicativosmagosso@gmail.com>
2020-07-09 00:13:05 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package br.victormagosso;
-
-public class Main {
-    public static void main(String ... args){
-        Bhaskara baskara = new Bhaskara();
-
-        baskara.setVisible(true);
-    }
-}
